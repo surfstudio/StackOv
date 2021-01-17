7b3298fb8bf72d95060b79dd24d2ef2270ecd7e8
Erik Basargin <basargin.erik@gmail.com>
2021-01-17 17:20:45 +0500

    Core Package.swift was fixed

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "StackOv",
     targets: [
         .binaryTarget(
-            name: "XCBeautify",
+            name: "xcbeautify",
             url: "https://github.com/thii/xcbeautify/releases/download/0.9.1/xcbeautify-0.9.1-universal-apple-macosx.zip",
             checksum: "c134c1f57fa7ca0efb35a159183176941f77e8e07c474c82be2da692ac9482fd"
         )]
