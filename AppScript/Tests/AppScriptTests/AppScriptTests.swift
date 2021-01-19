fcc237d4c487252b650f1757385dfcbd5233ca1f
Erik Basargin <basargin.erik@gmail.com>
2021-01-19 14:48:40 +0500

    Test were restored

--- AppScript/Tests/AppScriptTests/AppScriptTests.swift
+++ AppScript/Tests/AppScriptTests/AppScriptTests.swift
@@ -3,8 +3,7 @@ import XCTest
 
 final class AppScriptTests: XCTestCase {
     func testExample() {
-        XCTAssertEqual(1, 1)
-//        XCTFail("Need add tests")
+        XCTFail("Need add tests")
     }
 
     static var allTests = [
--- Stores/PageStore/Tests/PageStoreTests/PageStoreTests.swift
+++ Stores/PageStore/Tests/PageStoreTests/PageStoreTests.swift
@@ -3,8 +3,7 @@ import XCTest
 
 final class PageStoreTests: XCTestCase {
     func testExample() {
-        XCTAssertEqual(1, 1)
-//        XCTFail("Need add tests")
+        XCTFail("Need add tests")
     }
 
     static var allTests = [
