7bf8a7d3163acf4eab4e2b0c6edfc341212099ea
Vlad <klimov@surfstudio.ru>
2021-03-17 18:39:11 +0300

    Created PostItemLoadingView

---
+++ Flows/HomeFlow/Sources/HomeFlow/PostItemLoadingView/PostItemLoadingView.swift
@@ -0,0 +1,74 @@
+//
+//  [NAME].swift
+//  StackOv ([NAME] module)
+//
+//  Created by Влад Климов
+//  Copyright © 2021 Erik Basargin. All rights reserved.
+//
+
+import SwiftUI
+import Palette
+
+struct PostItemLoadingView: View {
+
+    // MARK: - Properties
+    
+    // MARK: - View
+    
+    var body: some View {
+        items
+            .frame(minWidth: 267, maxWidth: .infinity, minHeight: 223, alignment: .leading)
+            .background(Palette.bluishwhite)
+            .cornerRadius(20)
+    }
+    
+    var items: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            topItems
+            centerItems
+            Spacer()
+            Rectangle()
+                .background(Palette.gainsboro)
+                .cornerRadius(12)
+                .frame(width: 80, height: 10)
+        }
+        .padding(EdgeInsets.all(16))
+    }
+    
+    var topItems: some View {
+        HStack(alignment: .center, spacing: 20) {
+            Rectangle()
+                .background(Palette.gainsboro)
+                .cornerRadius(10)
+                .frame(width: 52, height: 41)
+            Rectangle()
+                .background(Palette.gainsboro)
+                .cornerRadius(12)
+                .frame(width: 48, height: 10)
+        }
+    }
+    
+    var centerItems: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Rectangle()
+                .background(Palette.gainsboro)
+                .cornerRadius(12)
+                .frame(width: 219, height: 10)
+            Rectangle()
+                .background(Palette.gainsboro)
+                .cornerRadius(12)
+                .frame(width: 92, height: 10)
+            Rectangle()
+                .background(Palette.gainsboro)
+                .cornerRadius(12)
+                .frame(width: 143, height: 10)
+        }
+    }
+    
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostItemLoadingView()
+    }
+}
