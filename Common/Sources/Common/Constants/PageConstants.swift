4ca393e8f53ec6b3976760711e212c8ac2281f20
Vlad Klimov <klimov-vd@yandex.ru>
2021-06-24 17:30:20 +0300

    Added Constants

---
+++ Common/Sources/Common/Constants/PageConstants.swift
@@ -0,0 +1,20 @@
+//
+//  [NAME].swift
+//  StackOv ([NAME] module)
+//
+//  Created by Владислав Климов
+//  Copyright © 2021 Erik Basargin. All rights reserved.
+//
+
+import Foundation
+import UIKit.UIDevice
+import struct UIKit.CGFloat
+
+public enum PageConstrants {
+    
+    public static let gridItemMinimumWidth: CGFloat = 267
+    public static let gridItemMaximumHeight: CGFloat = 223
+    public static var defaultSpacing: CGFloat {
+        UIDevice.current.userInterfaceIdiom.isPad ? 18 : 12
+    }
+}
---
+++ Common/Sources/Common/Constants/ThreadItemConstants.swift
@@ -0,0 +1,14 @@
+//
+//  [NAME].swift
+//  StackOv ([NAME] module)
+//
+//  Created by Владислав Климов
+//  Copyright © 2021 Erik Basargin. All rights reserved.
+//
+
+import struct UIKit.CGFloat
+
+public enum ThreadItemConstants {
+    
+    public static let defaultPadding: CGFloat = 16
+}
